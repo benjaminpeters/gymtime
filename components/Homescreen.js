c404825f6c68b54a154aa162bc960a9c5af103af
Benjamin <benpeters49@gmail.com>
2018-09-12 22:07:08 -0400

    Added header button to homescreen - will need to a redux to commuicate between the header and the screen

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -2,25 +2,35 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 
 class HomeScreen extends React.Component {
-    static navigationOptions = {
-      title: 'GYMTIME',
-    };
+
+    componentDidMount() {
+        this.props.navigation.setParams({ onAddWorkout: this.onAddWorkout });
+      }
+
+      static navigationOptions = ({ navigation }) => {
+        return {
+            title: 'GYMTIME',
+            headerRight: (
+                <Button
+                    onPress={navigation.getParam('onAddWorkout')}
+                    title="Add"
+                    color="#fff"
+                />
+            ),
+            };
+        }
+
+    onAddWorkout = () => {
+        this.props.navigation.navigate('Workout', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+        });
+    }
     
-  
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>Home Screen</Text>
-          <Button
-            title="Add Workout"
-            onPress={() => {
-              /* 1. Navigate to the Details route with params */
-              this.props.navigation.navigate('Workout', {
-                itemId: 86,
-                otherParam: 'anything you want here',
-              });
-            }}
-          />
         </View>
       );
     }
